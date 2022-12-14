74cdeb88b8e0be665cad7cce54b8d80168389e15
hanhhong.nguyen <hanhhong.nguyen@isobar.com>
2022-12-14 18:29:50 +0700

    Bus Service BO: Part 1.5: findOne, validate

--- middlewares/validate/authorize.js
+++ middlewares/validate/authorize.js
@@ -1,19 +1,21 @@
+const { User } = require("../../services/index.service");
+
 const authorize = (typeArr) => {
   return async (req, res, next) => {
-    next();
-  //   const { user } = req;
-  //   const userData = await User.findOne({
-  //     include: [{ model: userType, as: "typeObj" }],
-  //     where: {
-  //       email: user.email,
-  //     },
-  //   });
-  //   const index = typeArr.findIndex((i) => i === userData.typeObj.name);
-  //   if (index !== -1) {
-  //     next();
-  //   } else {
-  //     res.status(403).send({ message: "Error Authorize" });
-  //   }
+    const { user } = req;
+    const userData = await User.findOne({
+      include: [{ model: userType, as: "typeObj", map: "typeId" }],
+      where: {
+        key: "email",
+        value: user.email,
+      },
+    });
+    const index = typeArr.findIndex((i) => i === userData.typeObj.name);
+    if (index !== -1) {
+      next();
+    } else {
+      res.status(403).send({ message: "Error Authorize" });
+    }
   };
 };
 
